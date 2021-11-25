23e9282586b14828bf452128611d43990c9e5856
hainguyen2k <ngxuanhai1000@gmail.com>
2021-11-25 20:11:54 +0700

    Add Discovery Service

---
+++ src/main/java/com/example/subproject_kttkpm_14btt_haibaotunghiahuy_discovery/SubprojectKttkpm14BttHaiBaoTuNghiaHuyDiscoveryApplication.java
@@ -0,0 +1,15 @@
+package com.example.subproject_kttkpm_14btt_haibaotunghiahuy_discovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class SubprojectKttkpm14BttHaiBaoTuNghiaHuyDiscoveryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SubprojectKttkpm14BttHaiBaoTuNghiaHuyDiscoveryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/subproject_kttkpm_14btt_haibaotunghiahuy_discovery/SubprojectKttkpm14BttHaiBaoTuNghiaHuyDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.subproject_kttkpm_14btt_haibaotunghiahuy_discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubprojectKttkpm14BttHaiBaoTuNghiaHuyDiscoveryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
